f9c38f5f7ab16a407eb01d41f0da251145aaa0fd
manikiranp <manikiranp3@gmail.com>
2020-11-30 19:44:21 +0530

    created linkedlist with three values and passed test case

--- src/datastructures/MyNodeTest.java
+++ src/datastructures/MyNodeTest.java
@@ -4,6 +4,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class MyNodeTest {
+	
 	@Test
 	public void given3num_whenlinkedshouldpasslinkedlisttest() {
 		MyNode<Integer> myfirstnode = new MyNode<>(56);
